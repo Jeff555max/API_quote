c783c7a80509323b05de951dd2569791922f7908
Jeff555max <vadik98765@gmail.com>
2025-01-18 15:25:37 +0500

    1

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import requests
-#import certifi
-#библиотеку certifi чтобы гарантировать, что Python использует доверенные корневые сертификаты
+# import certifi
+# библиотека certifi, чтобы гарантировать, что Python использует доверенные корневые сертификаты
 # Установить библиотеку, если она у вас не установлена
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 @app.route('/', methods=['GET'])
 def randomQuote():
     try:
-        #без verify возникла Ошибка SSLCertVerificationError: certificate verify failed: certificate has expired
+        # без verify возникла Ошибка SSLCertVerificationError: certificate verify failed: certificate has expired
         # при запросе к API https://api.quotable.io/random говорит о том, что возникли проблемы
         # с проверкой SSL-сертификата при подключении к серверу. Это означает, что компьютер (или среда,
         # в которой запущено приложение) не может должным образом проверить подлинность сертификата сервера api.quotable.io
@@ -22,7 +22,7 @@ def randomQuote():
         # Использовать его нужно только для отладки и тестирования, но не в производственной среде
 
         response.raise_for_status()  # Проверка на ошибки HTTP
-        data = response.json() #Извлекаем данные из JSON-ответа
+        data = response.json() # Извлекаем данные из JSON-ответа
         quote = data.get('content', 'Не удалось получить цитату')
         author = data.get('author', 'Неизвестный автор')
         return render_template('randomQuote.html', quote=quote, author=author)
