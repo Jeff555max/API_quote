3713cada916a1239cde3f38766c65143b81a436e
Евгений Богачев <178380687+Jeff555max@users.noreply.github.com>
2025-09-10 14:50:50 +0500

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import requests
 #import certifi
 #библиотеку certifi чтобы гарантировать, что Python использует доверенные корневые сертификаты
-# Установить библиотеку, если она у вас не установлена
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -11,16 +10,7 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 @app.route('/', methods=['GET'])
 def randomQuote():
     try:
-        #без verify возникла Ошибка SSLCertVerificationError: certificate verify failed: certificate has expired
-        # при запросе к API https://api.quotable.io/random говорит о том, что возникли проблемы
-        # с проверкой SSL-сертификата при подключении к серверу. Это означает, что компьютер (или среда,
-        # в которой запущено приложение) не может должным образом проверить подлинность сертификата сервера api.quotable.io
-
-        #response = requests.get('https://api.quotable.io/random', verify=certifi.where()) - указывает путь к сертификатам явно, но тут не пошел
-        response = requests.get('https://api.quotable.io/random', verify = False) #Это небезопасный способ,
-        # поскольку делает приложение уязвимым для атак “человек посередине”.
-        # Использовать его нужно только для отладки и тестирования, но не в производственной среде
-
+        response = requests.get('https://api.quotable.io/random', verify = False) 
         response.raise_for_status()  # Проверка на ошибки HTTP
         data = response.json() #Извлекаем данные из JSON-ответа
         quote = data.get('content', 'Не удалось получить цитату')
